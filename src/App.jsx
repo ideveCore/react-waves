6fd7cc5ed6271e5bf00b771f09c1080cc2c9ae9b
Ideve Core <ideve.core@gmail.com>
2024-07-13 12:57:53 -0300

    Remove unused contents

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+/**
+ * Create Application component
+ *
+ * @component
+ * @param {Object} props - The component accepts
+ * @returns {JSX.Element} The rendered component.
+ *
+ * @example
+ * // Render a App component
+ * <App/>
+ *
+ */
 export const App = ({}) => {
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 
+/**
+ *
+ * Render the react application in #root html element
+ *
+ */
 ReactDOM.createRoot(document.querySelector("#root")).render(
   <React.StrictMode>
     <App />
