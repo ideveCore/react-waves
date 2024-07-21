bfc2c640ab0d49726d5fa704a29b01d113daa9d3
Ideve Core <ideve.core@gmail.com>
2024-07-21 18:49:30 -0300

    Added PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'Waves',
+        name: 'Waves',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        theme_color: '#FFDB00',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+        ],
+        screenshots: [
+          {
+            src: "/screenshots/01.png",
+            typy: "image/png",
+            sizes: "540x720",
+            form_factor: "narrow"
+          },
+          {
+            src: "/screenshots/02.png",
+            typy: "image/png",
+            sizes: "720x540",
+            form_factor: "wide"
+          }
+        ],
+      },
+    })],
 })
